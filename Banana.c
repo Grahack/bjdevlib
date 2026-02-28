c9b0ccc0181a52b40b9a24446fe13bbdd8934ee5
Christophe Gragnic <christophegragnic@gmail.com>
2026-02-28 18:02:03 +0100

    Quite some work for the Mutes thing

--- Banana.c
+++ Banana.c
@@ -25,7 +25,13 @@
  *    * 4/1 is Up/Down, 2 is Left+Play (to cue a row)
  *    * 5 is just Play but also used to stop playback
  *    * 3 is Mute the selected tracks (use the arrows, see below)
- *  * <> to scroll through the tracks, down to prepare for a Mute
+ *  * <> to scroll through the tracks, down to prepare for a Mute/Unmute
+ *                                     up to prepare for an Unmute/mute
+ *                                     middle pos is do not touch this track
+ *  +----------------+
+ *  |        12345678|  Currently editing track 2 (shows MUX instead of v^-).
+ *  |        -M-v--^-|  On button 3 switch: mute track 4, unmute track 7.
+ *  +----------------+
  */
 
 // M8 keys to MIDI notes (Play Shift Edit Option Left Right Up Down)
@@ -37,6 +43,38 @@
 #define M8_R 5
 #define M8_U 6
 #define M8_D 7
+
+// TB-5 setup buttons
+#define TB_R 5
+#define TB_L 6
+#define TB_U 7
+#define TB_D 8
+#define TB_O 9
+#define TB_N 10
+
+// Display positions
+uint8_t xTextForHandButtons = 8;
+uint8_t yTextForHandButtons = 1;
+uint8_t xTextForFootButtons = 0;
+uint8_t yTextForFootButtons = 0;
+// M8 tracks handling
+bool tracks_switch_state = false;  // start unswitched
+uint8_t tracks_currently_edited = 8;  // 8 is "track 9" (hides the MUX)
+bool tracks_states[8] = {true, true, true, true, true, true, true, true};
+// 0=mute on switch, 1=do not touch, 2=unmute on switch
+uint8_t tracks_conf[8] = {1, 1, 1, 1, 1, 1, 1, 1};
+// relevant MIDI notes
+int M8_mute_notes[8] = {12, 13, 14, 15, 16, 17, 18, 19};
+// Trigger handling
+uint8_t state = 0;    // 0=idle, 1=looking for peak, 2=ignore aftershocks
+uint8_t maxRead = 0;  // used to detect the peak of the piezo signal
+uint16_t refresh_kick = 25500; // number of loops to wait for next detection
+uint16_t ticks_kick  = 0;      // if >0 we have to wait
+uint8_t v1min = 221;  // min value read
+uint8_t v1max = 255;  // max value read
+uint8_t v2min = 10;   // min MIDI velocity
+uint8_t v2max = 127;  // max MIDI velocity
+uint8_t margin = 15;  // piezo is very sensitive, used to prevent false +
 
 void expPedalsCallback(PedalNumber n, uint8_t pos)
 {
@@ -50,6 +88,82 @@ void expPedalsCallback(PedalNumber n, uint8_t pos)
     LCDWriteIntXY(4, 1, pos, 3);
 }
 
+void updateMutesAfterButtons()
+{
+    for (int i=0; i<8; i++)
+    {
+        uint8_t track_conf = tracks_conf[i];
+        if (i == tracks_currently_edited) track_conf = track_conf + 10;
+        LCDGotoXY(xTextForHandButtons + i, yTextForHandButtons);
+        switch (track_conf)
+        {
+            case 0:
+                LCDWriteString("v");
+                break;
+            case 1:
+                LCDWriteString("-");
+                break;
+            case 2:
+                LCDWriteString("^");
+                break;
+            case 10:
+                LCDWriteString("M");
+                break;
+            case 11:
+                LCDWriteString("X");
+                break;
+            case 12:
+                LCDWriteString("U");
+                break;
+        }
+    }
+}
+
+void updateAfterButtons(uint8_t buttonNumber, uint8_t status)
+{
+    if (status == BUTTON_PUSH)
+    {
+        if (buttonNumber < 5)
+        {
+            LCDGotoXY(xTextForFootButtons, yTextForFootButtons);
+            char* display[5] = {"DOWN   ",
+                                "CUE    ",
+                                "SW-T   ",
+                                " UP    ",
+                                "PLAY   "};
+            LCDWriteString(display[buttonNumber]);
+        }
+        else if (buttonNumber == TB_U)
+        {
+            if (tracks_conf[tracks_currently_edited] < 2)
+                tracks_conf[tracks_currently_edited]++;
+            updateMutesAfterButtons();
+        }
+        else if (buttonNumber == TB_D)
+        {
+            if (tracks_conf[tracks_currently_edited] > 0)
+                tracks_conf[tracks_currently_edited]--;
+            updateMutesAfterButtons();
+        }
+        else if (buttonNumber == TB_L || buttonNumber == TB_R)
+        {
+            updateMutesAfterButtons();
+        }
+    }
+    else if (status == BUTTON_RELEASE)
+    {
+        if (buttonNumber < 5)
+        {
+            LCDGotoXY(xTextForFootButtons, yTextForFootButtons);
+            LCDWriteString("BotBoss");
+        }
+    }
+    else
+    {
+        LOG(SEV_INFO, "Unknown button status");
+    }
+}
+
 int main(void)
 {
     initBjDevLib();
@@ -58,9 +172,11 @@ int main(void)
 
     LCDInit(LS_ULINE);
     LcdHideCursor();
-    LCDWriteString((char*)"   Banana FC");
+    // "BotBoss" is meant to be overwritten on first foot press.
+    LCDWriteString((char*)"BotBoss 12345678");
     LCDGotoXY(0, 1);
     LCDWriteString((char*)"EXP:");
+    updateMutesAfterButtons();
     
     ledSetColorAll(COLOR_BLACK,  true);
     ledSetColor(0, COLOR_YELLOW, true);
@@ -70,22 +186,6 @@ int main(void)
     ledSetColor(4, COLOR_RED,    true);
 
     ButtonEvent lastButtonEvent;
-    
-    // Display positions
-    uint8_t xTextForButtons = 8;
-    // Trigger handling
-    uint8_t state = 0;    // 0=idle, 1=looking for peak, 2=ignore aftershocks
-    uint8_t maxRead = 0;  // used to detect the peak of the piezo signal
-    uint8_t refresh = 255; // number of loops to wait for next detection
-    uint8_t ticks = 0;    // if >0 we have to wait
-    uint8_t v1min = 221;  // min value read
-    uint8_t v1max = 255;  // max value read
-    uint8_t v2min = 10;   // min MIDI velocity
-    uint8_t v2max = 127;  // max MIDI velocity
-    uint8_t margin = 15;  // piezo is very sensitive, used to prevent false +
-    uint8_t v1gap = v1max-v1min;  // for curve calculations
-    uint8_t v2gap = v2max-v2min;
-
 
     while (1)
     {
@@ -94,10 +194,6 @@ int main(void)
         {
             LOG(SEV_INFO, "Debug works : %d", 2+3);
             uint8_t buttonNumber = lastButtonEvent.buttonNum_;
-            LCDGotoXY(xTextForButtons, 1);
-            char* display[11] = {"DOWN", "CUE ", "SUST", " UP ", "PLAY",
-                                "5   ", "6   ", "7   ", "8   ", "9   ", "10  "};
-            LCDWriteString(display[buttonNumber]);
             switch (buttonNumber)
             {
                 case 0:  // button 1
@@ -122,8 +218,10 @@ int main(void)
                     midiSendNoteOff(M8_P, 127, M8_CHANNEL);
                     break;
                 case 5:  // Inc or Right
+                    if (tracks_currently_edited < 8) tracks_currently_edited++;
                     break;
                 case 6:  // Dec or Left
+                    if (tracks_currently_edited > 0) tracks_currently_edited--;
                     break;
                 case 7:  // Up
                     break;
@@ -134,12 +232,11 @@ int main(void)
                 case 10: // Setup / No
                     break;
             }
+            updateAfterButtons(buttonNumber, BUTTON_PUSH);
         }
         else if(lastButtonEvent.actionType_ == BUTTON_RELEASE)
         {
             uint8_t buttonNumber = lastButtonEvent.buttonNum_;
-            LCDGotoXY(xTextForButtons, 1);
-            LCDWriteString("    ");
             switch (buttonNumber)
             {
                 case 0:
@@ -154,6 +251,7 @@ int main(void)
                 case 4:
                     break;
             }
+            updateAfterButtons(buttonNumber, BUTTON_RELEASE);
         }
 
         // Read EXP P1 port to handle the exp pedal
@@ -191,6 +289,8 @@ int main(void)
             // we have a peak so we send MIDI
             uint8_t velo = 0;
             // I wanted to compute velo between v2min and v2max
+            const uint8_t v1gap = v1max-v1min;
+            const uint8_t v2gap = v2max-v2min;
             // but I can only detect 3 states :/
             velo = v2gap;  // dummy, just to avoid compiler warnings
             if(maxRead < v1min + 4*v1gap/5)
@@ -208,15 +308,15 @@ int main(void)
             LCDWriteString("KICK");
             midiSendNoteOff(38, 0, M8_CHANNEL);
             // and we go to Ignore aftershock state
-            ticks = refresh;
+            ticks_kick = refresh_kick;
             state = 2;
         }
         break;
 
         // Ignore Aftershock state: wait for things to be quiet again.
         default:
-            ticks--;
-            if(ticks == 0)
+            ticks_kick--;
+            if(ticks_kick == 0)
             {
                 // go back to idle
                 state = 0;
